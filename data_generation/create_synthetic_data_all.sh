786f83e0c680891ae0130fdc1e15d81b3f364c1e
AH-20 <93234736+AH-20@users.noreply.github.com>
2025-08-23 19:46:04 -0400

    updates

--- data_generation/create_synthetic_data_all.sh
+++ data_generation/create_synthetic_data_all.sh
@@ -9,28 +9,28 @@
 
 
 echo "Generating RCT Data"
-bash data_generation/create_rct_data.sh
+bash data_generation/create_data/create_rct_data.sh
 
 echo "Generating Multi-RCT Data"
-bash data_generation/create_multi_rct_data.sh
+bash data_generation/create_data/create_multi_rct_data.sh
 
 echo "Generating Front_Door Data"
-bash data_generation/create_front_door_data.sh
+bash data_generation/create_data/create_front_door_data.sh
 
 echo "Generating Observational Data"
-bash data_generation/create_observational_data.sh
+bash data_generation/create_data/create_observational_data.sh
 
 echo "Generating Canonical DiD Data"
-bash data_generation/create_did_canonical_data.sh
+bash data_generation/create_data/create_did_canonical_data.sh
 
 echo "Generating TWFE DiD Data"
-bash data_generation/create_did_twfe_data.sh
+bash data_generation/create_data/create_did_twfe_data.sh
 
 echo "Generating IV Data"
-bash data_generation/create_iv_data.sh
+bash data_generation/create_data/create_iv_data.sh
 
 echo "Generating IV-Encouragement Data"
-bash data_generation/create_iv_encouragement_data.sh
+bash data_generation/create_data/create_iv_encouragement_data.sh
 
 echo "Generating RDD Data"
-bash data_generation/create_rdd_data.sh
+bash data_generation/create_data/create_rdd_data.sh
